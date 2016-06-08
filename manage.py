4cefa5e79a976a3de2b9bfb8f108b7e0e4e94076
Ben Baert <ben_b@gmx.com>
2016-06-08 20:44:12 +0200

    Removed obsolete celery commands from manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
 from manager import Manager
 from realestate import app
-from realestate.celery import (generate_feed, prepare_caches, add_realo_realestate)
 from realestate.models import (Realestate,
                            UserRealestateReview)
 
@@ -9,22 +8,6 @@ manager = Manager()
 @manager.command
 def run_test_server():
     app.run(debug=True)
-
-
-@manager.command
-def generate_realestate_feed():
-    return generate_feed()
-
-
-@manager.command
-def prepare_queue():
-    return prepare_caches()
-
-
-@manager.arg('url')
-@manager.command
-def add_realo_realestate_by_url(url):
-    return add_realo_realestate(url)
 
 
 @manager.arg('property_id', help='The id of the property you want all reviews to be removed for')
@@ -42,5 +25,7 @@ def remove_user_reviews_for_property(property_id):
     property_reviews_delete_query.execute()
     print("Property reviews for property {} deleted!".format(property_id))
 
+
 if __name__ == '__main__':
     manager.main()
+
